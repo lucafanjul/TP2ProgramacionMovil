43ca9554acce010207558d98b6dab668f0f61aa8
pedro <pedro@LUCAFANJULPC>
2026-05-08 02:37:42 -0300

    Agregar archivos de proyecto.

---
+++ TP2ProgramacionMovil/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TP2ProgramacionMovil.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "El usuario es obligatorio")]
+        public string Usuario { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        public string Contrasena { get; set; } = string.Empty;
+    }
+}
---
+++ TP2ProgramacionMovil/Models/Persona.cs
@@ -0,0 +1,12 @@
+﻿namespace TP2ProgramacionMovil.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Domicilio { get; set; } = string.Empty;
+        public DateTime FechaNacimiento { get; set; }
+        public string RutaImagen { get; set; } = string.Empty;
+    }
+}
